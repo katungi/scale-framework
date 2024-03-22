05f97f22110ef6a53522e61579956a89409e69af
katungi <dankatdennis@gmail.com>
2024-03-22 08:45:50 +0300

    feat: adds a file server to serve files

--- deps.ts
+++ deps.ts
@@ -1,4 +1,11 @@
+// Import deps for Compiler and Analyzer
 export * as acorn from "npm:acorn@8.11.3"
 export * as escodegen from "npm:escodegen@2.1.0"
 export * as estreewalker from "npm:estree-walker@3.0.3"
 export * as periscopic from "npm:periscopic@4.0.2"
+
+// Server stuff
+export { serve } from "https://deno.land/std@0.107.0/http/server.ts";
+export { existsSync } from "https://deno.land/std@0.107.0/fs/mod.ts";
+export { resolve, toFileUrl } from "https://deno.land/std@0.107.0/path/mod.ts";
+export { serveDir, serveFile } from "https://deno.land/std@0.207.0/http/file_server.ts";
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { Parser } from "./compiler/parser.ts";
 import { Analyze } from "./compiler/analyses.ts";
 import { Generate } from "./compiler/generator.ts";
+import { serveDir, serveFile } from "./deps.ts";
+import { join } from "https://deno.land/std@0.107.0/path/win32.ts";
 
 const decode = new TextDecoder("utf-8")
 const fileContent = Deno.readFileSync("./app/app.scale")
@@ -13,4 +15,17 @@ const analysis = Analyze(parsed)
 
 const generatedCode = Generate(parsed, analysis)
 
-Deno.writeFileSync("./app/app.gen.js", new TextEncoder().encode(generatedCode))
+Deno.writeTextFileSync("./app/app.gen.js", generatedCode)
+
+Deno.serve((req: Request) => {
+  const p = new URL(req.url).pathname
+  
+  if (p === '/') {
+    return serveFile(req, './app/index.html')
+  } else if (p === '/app.gen.js') {
+    return serveFile(req, './app/app.gen.js')
+  }
+
+  return new Response('Not found', { status: 404 })
+})
+
